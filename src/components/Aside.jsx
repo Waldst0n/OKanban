3e0f6baf251fbddcb574fdd2be27124412fc1148
Waldston <waldston.ws@gmail.com>
2024-10-22 06:10:49 -0300

    feat: add Asisde component

---
+++ src/components/Aside.jsx
@@ -0,0 +1,9 @@
+const Aside = () => {
+  return (
+    <aside className="w-1/5  rounded-tr-xl shadow  h-screen bg-boardColor">
+      <h1 className="text-2xl">Aside</h1>
+    </aside>
+  );
+};
+
+export default Aside;
