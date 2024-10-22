e0485cb25b638c0733bcb75e808a2cce421a1d82
Waldston <waldston.ws@gmail.com>
2024-10-22 06:11:02 -0300

    feat: add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div>
+      <header className="w-full  size-12 p-6 flex items-center ">
+        <h3>OKanban</h3>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
