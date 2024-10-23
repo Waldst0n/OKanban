2c8926694eabb5059802643bdb74de6f6301dd46
Waldston <waldston.ws@gmail.com>
2024-10-23 11:52:55 -0300

    refactor: Alteration on Aside

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,6 +1,6 @@
 const Aside = () => {
   return (
-    <aside className="w-1/5  rounded-tr-xl shadow  h-screen bg-boardColor">
+    <aside className="min-w-56 p-4  rounded-tr-xl shadow  h-screen bg-boardColor">
       <h1 className="text-2xl">Aside</h1>
     </aside>
   );
