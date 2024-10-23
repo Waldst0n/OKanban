bbae2f33fce47d73c3610d6021e8d7acd85297b0
Waldston <waldston.ws@gmail.com>
2024-10-23 11:55:36 -0300

    chore: Created mock Database

---
+++ src/db/database.js
@@ -0,0 +1,24 @@
+const taskCards = [
+  {
+    id: 1,
+    name: 'Task 1',
+    status: 'done',
+  },
+  {
+    id: 2,
+    name: 'Task 2',
+    status: 'started',
+  },
+  {
+    id: 3,
+    name: 'Task 3',
+    status: 'stopped',
+  },
+  {
+    id: 4,
+    name: 'Task 4',
+    status: 'not-started',
+  },
+];
+
+export default taskCards;
