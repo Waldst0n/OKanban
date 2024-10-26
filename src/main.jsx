600ea38c6ab0fd2a7305c7750bac86364a5949be
Waldston <waldston.ws@gmail.com>
2024-10-26 11:31:40 -0300

    refactor: Move TaskProvider to Main Component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { TaskProvider } from './context/TaskContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <TaskProvider>
+      <App />
+    </TaskProvider>
+  </StrictMode>
+);
