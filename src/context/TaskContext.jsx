6bbd1981dc6c0eb5a4c071788019b1c0da56ec0c
Waldston <waldston.ws@gmail.com>
2024-10-25 21:26:01 -0300

    feat: Created initial Data to Tasks

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -3,14 +3,39 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const TaskContext = createContext();
 
+const initialTasks = [
+  {
+    id: 1,
+    name: 'task 1',
+    status: 'not-started',
+  },
+  {
+    id: 2,
+    name: 'task 2',
+    status: 'not-started',
+  },
+  {
+    id: 3,
+    name: 'task 3',
+    status: 'not-started',
+  },
+  {
+    id: 4,
+    name: 'task 4',
+    status: 'not-started',
+  },
+];
+
 export const TaskProvider = ({ children }) => {
   const [tasks, setTasks] = useState(() => {
     const savedTasks = localStorage.getItem('taskCard');
+    const initialTasksJson = JSON.stringify(initialTasks);
+
     return savedTasks ? JSON.parse(savedTasks) : [];
   });
 
   useEffect(() => {
-    localStorage.setItem('taskCard', JSON.stringify(tasks));
+    localStorage.setItem('taskCard', JSON.stringify(initialTasks));
   }, [tasks]);
 
   const updateTaskStatus = (taskName, newStatus) => {
